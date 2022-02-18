a47be166bb27e5b04c4d091c79344e2364a49658
Magdalena Huget <magdalena.huget@gmail.com>
2022-02-18 18:44:54 +0100

    Add empty game controller

---
+++ memoryGame/memoryGame/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace memoryGame
+{
+    using System;
+
+
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            GameController gameController = new GameController();
+            gameController.run();
+        }
+    }
+
+    public class GameController
+    {
+        public GameController()
+        {
+        }
+
+        public void run()
+        {
+            
+        }
+
+    }
+    
+}
