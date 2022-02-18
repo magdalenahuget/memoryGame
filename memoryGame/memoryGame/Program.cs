e347a9c5ce1fbb02e47db931746ba621f7a53efe
Magdalena Huget <magdalena.huget@gmail.com>
2022-02-18 20:29:39 +0100

    Add shuffle

--- memoryGame/memoryGame/Program.cs
+++ memoryGame/memoryGame/Program.cs
@@ -131,7 +131,7 @@ namespace memoryGame
                 list.Add(i);
             }
 
-            // shuffle(list); // TODO:
+            shuffle(list); 
             List<int> indexes = new List<int>();
             for (int i = 0; i < WORDS_NUMBER; i++)
             {
@@ -140,6 +140,21 @@ namespace memoryGame
             }
 
             return indexes;
+        }
+        
+        private static Random rng = new Random();
+
+        public static void shuffle<T>(IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
 
         public String getPlayerName()
